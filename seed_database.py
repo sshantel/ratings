f9154480e01b1e4794910cc22d630e4e3feb84d1
Chantel Yip <chantelyip422@gmail.com>
2020-05-29 13:02:15 -0700

    initial commit

---
+++ seed_database.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+os.system('dropdb ratings')
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
